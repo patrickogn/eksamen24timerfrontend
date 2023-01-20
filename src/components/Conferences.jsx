81781ef76f59b57f775baf0802be1504ce65fa1d
Patrick Nielsen <cph-pn157@cphbusiness.dk>
2023-01-20 02:57:00 +0100

    US2 Done

--- src/components/Conferences.jsx
+++ src/components/Conferences.jsx
@@ -1,16 +1,11 @@
-import facade from "../apiFacade";
 import React, { useEffect, useState } from "react";
 
-
 const Conferences = () => {
+  const [data, setData] = useState({}); // usestate holder dataen
+  const [conference, setConference] = useState(null); 
 
-const [conference, setConference] = useState(null); 
-
-
-useEffect(() => {
-    fetch(
-      "http://localhost:8080/24timers/api/conf/all",
-    )
+  useEffect(() => {
+    fetch("http://localhost:8080/24timers/api/conf/all")
       .then((response) => response.json())
       .then((data) => {
         setConference(data);
@@ -18,23 +13,31 @@ useEffect(() => {
       });
   }, []);
 
+  async function handleClick(id) {
+    const response = await fetch(`http://localhost:8080/24timers/api/conf/${id}`);
+    const json = await response.json();
+    setData({...data, [id]: json}); // opdaterer state med ny data
+  }
 
   return (
     <div>
-     {conference &&
-      conference.map((conference) => (
-        <div>
-          <ul>
-            <li>
-              Name: {conference.name}, Location: {conference.location},
-              Capacity: {conference.capacity}, Date: {conference.date}, Time: {conference.time}
-            </li> 
-          </ul>
-        </div>
-      ))}
-
-  </div>
-);
+      {conference &&
+        conference.map((conf) => (
+          <div key={conf.id}>
+            <ul>
+              <li>
+                Name: {conf.name}, Location: {conf.location},
+                Capacity: {conf.capacity}, Date: {conf.date}, Time: {conf.time}  
+                <div>
+                  <button onClick={() => handleClick(conf.id)}>See Talks</button>
+                  {data[conf.id] && <div>{JSON.stringify(data[conf.id])}</div>}
+                </div>
+              </li> 
+            </ul>
+          </div>
+        ))}
+    </div>
+  );
 };
 
-export default Conferences
+export default Conferences;
